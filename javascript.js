9210ebd8bc7161ab48b26e5e51be86bc7a4ea3e1
TKCoder13 <TKCoding13@gmail.com>
2023-07-24 19:07:49 -0700

    Complete RPS Project Part 1

--- javascript.js
+++ javascript.js
@@ -1,35 +1,38 @@
 function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3);
-    console.log(rand);
-    if (rand === 1) {
+    if (rand == 1) {
         return "Rock";
-    } else if (rand === 2) {
+    } else if (rand == 2) {
         return "Paper";
     } else {
         return "Scissors";
     }
 }
 
-let compHand = getComputerChoice();
-let playerHand = window.prompt("Let's play RPS! What do you choose?");
+function getPlayerChoice() {
+    return prompt("Let's play RPS! What do you choose?");
+}
 
-function playerSelection(playerHand, compHand) {
-    if (playerHand === compHand) {
+let compHand = getComputerChoice().toLowerCase();
+let playerHand = getPlayerChoice().toLowerCase();
+
+function playRound() {
+    if (playerHand == compHand) {
         return "It's a draw!";
-    } else if (playerHand === "Rock") {
-        if (compHand === "Scissors") {
+    } else if (playerHand == "rock") {
+        if (compHand == "scissors") {
             return "You Win! Rock beats Scissors";
         } else {
             return "You Lose :( Paper beats Rock";
         }
-    } else if (playerHand === "Paper") {
-        if (compHand === "Rock") {
+    } else if (playerHand == "paper") {
+        if (compHand == "rock") {
             return "You Win! Paper beats Rock";
         } else {
             return "You Lose :( Scissors beats Paper";
         }
-    } else if (playerHand === "Scissors") {
-        if (compHand === "Paper") {
+    } else if (playerHand == "scissors") {
+        if (compHand == "paper") {
             return "You Win! Scissors beats Paper";
         } else {
             return "You Lose :( Rock beats Scissors";
@@ -38,6 +41,6 @@ function playerSelection(playerHand, compHand) {
         return "Misinput! Try again!";
     }
 }
-console.log("CompHand = " + compHand);
-console.log("PlayerHand = " + playerHand);
-console.log(playerSelection());
+// console.log("CompHand = " + compHand);
+// console.log("PlayerHand = " + playerHand);
+console.log(playRound());
